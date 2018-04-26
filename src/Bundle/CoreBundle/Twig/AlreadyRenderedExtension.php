13dd6c87fe3e59155ee4197d71ca8cd1d1ab78b1
Franz Wilding <franz.wilding@me.com>
2018-04-26 17:01:15 +0200

    Fix #45 [CoreBundle, CollectionFieldBundle, StorageBundle, WysiwygBundle]: Only include form assets once in a page

---
+++ src/Bundle/CoreBundle/Twig/AlreadyRenderedExtension.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: franzwilding
+ * Date: 26.04.18
+ * Time: 16:38
+ */
+
+namespace UniteCMS\CoreBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig_SimpleFunction;
+
+class AlreadyRenderedExtension extends AbstractExtension
+{
+    private $renderedKeys = [];
+
+    public function getFunctions()
+    {
+        return [
+            new Twig_SimpleFunction('alreadyRendered', [$this, 'alreadyRendered']),
+        ];
+    }
+
+    /**
+     * Returns true, if this method was already called with this key. This is useful to render twig blocks exactly once
+     * on a page. We use this in form theme blocks to include css and js files only once.
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function alreadyRendered(string $key) : bool {
+
+        if(!isset($this->renderedKeys[$key])) {
+            $this->renderedKeys[$key] = true;
+            return false;
+        }
+
+        return true;
+    }
+
+}
