d2e93fe423579db8ab58db63e5db938faedaa80c
Franz Wilding <franz.wilding@me.com>
2018-05-16 17:50:09 +0200

    feature #66 [CoreBundle]: Implement twig instanceof test and use it to determine if a domain member is an api key or an user

---
+++ src/Bundle/CoreBundle/Twig/InstanceOfExtension.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: franzwilding
+ * Date: 26.04.18
+ * Time: 16:38
+ */
+
+namespace UniteCMS\CoreBundle\Twig;
+
+use ReflectionClass;
+use Twig\Extension\AbstractExtension;
+use Twig_SimpleTest;
+
+class InstanceOfExtension extends AbstractExtension
+{
+    public function getTests()
+    {
+        return [
+            new Twig_SimpleTest('instanceof', [$this, 'isInstanceOf']),
+        ];
+    }
+
+    /**
+     * Returns true, if $object is an instance of $class.
+     *
+     * @param object $object
+     * @param string $class
+     * @return bool
+     *
+     * @throws \ReflectionException
+     */
+    public function isInstanceOf($object, string $class) : bool {
+        $reflectionClass = new ReflectionClass($class);
+        return $reflectionClass->isInstance($object);
+    }
+
+}
