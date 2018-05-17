7437d1e2ebd95476634b51650372f088c94637b7
Franz Wilding <franz.wilding@me.com>
2018-05-17 17:50:06 +0200

    feature #54 [CoreBundle]: Implement an access expression checker to check expressions with domainMember and fieldableContent objects

---
+++ src/Bundle/CoreBundle/Security/PlainExpressionLanguage.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: franzwilding
+ * Date: 17.05.18
+ * Time: 16:40
+ */
+
+namespace App\Bundle\CoreBundle\Security;
+
+
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+
+/**
+ * The plain expression language don't provide any functions and can only decide based on variables and basic syntax
+ * elements.
+ */
+class PlainExpressionLanguage extends ExpressionLanguage
+{
+    /**
+     * Don't register the constant function.
+     */
+    protected function registerFunctions()
+    {}
+}
