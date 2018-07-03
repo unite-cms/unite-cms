d7ccb9ad5a70d3ffa3ac772c89ec76cb5f7f6725
Franz Wilding <franz.wilding@me.com>
2018-07-03 16:29:42 +0200

    feature #163 [WysiwygFieldBundle]: Improve heading settings to allow to add custom classes and models

--- src/Bundle/WysiwygFieldBundle/Resources/public/main.6fded86d7bed1654ba83.js
+++ src/Bundle/WysiwygFieldBundle/Resources/public/main.6fded86d7bed1654ba83.js
@@ -12192,43 +12192,8 @@ if (false) {(function () {
 
 /* harmony default export */ __webpack_exports__["a"] = ({
     data: function data() {
-
-        var options = JSON.parse(this.dataOptions);
-
-        if (options.heading) {
-            options.toolbar.unshift('|');
-            options.toolbar.unshift('heading');
-            options.heading = options.heading.map(function (heading) {
-
-                if (heading === 'p') {
-                    return {
-                        model: 'paragraph',
-                        view: heading,
-                        title: 'Paragraph',
-                        class: 'ck-heading_paragraph'
-                    };
-                }
-
-                var headlineMatch = heading.match(/h([1-6]+)/);
-                if (headlineMatch) {
-                    return {
-                        model: 'heading' + headlineMatch[1],
-                        view: heading,
-                        title: 'Heading ' + headlineMatch[1],
-                        class: 'ck-heading_heading' + headlineMatch[1]
-                    };
-                }
-
-                return {
-                    model: heading,
-                    view: heading,
-                    title: heading
-                };
-            });
-        }
-
         return {
-            'options': options,
+            'options': JSON.parse(this.dataOptions),
             'content': this.value
         };
     },
