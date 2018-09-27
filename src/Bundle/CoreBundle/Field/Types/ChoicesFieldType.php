8f00134ed2819b4b79d5cd82e2e66c28588d3102
Franz Wilding <franz.wilding@me.com>
2018-09-27 10:01:02 +0200

    Implement custom choices type that is based on choice type

---
+++ src/Bundle/CoreBundle/Field/Types/ChoicesFieldType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace UniteCMS\CoreBundle\Field\Types;
+
+use GraphQL\Type\Definition\Type;
+use UniteCMS\CoreBundle\Entity\FieldableField;
+use UniteCMS\CoreBundle\SchemaType\SchemaTypeManager;
+
+class ChoicesFieldType extends ChoiceFieldType
+{
+    const TYPE = "choices";
+
+    function getFormOptions(FieldableField $field): array
+    {
+        return array_merge(
+            parent::getFormOptions($field),
+            [
+                'multiple' => true,
+                'expanded' => true,
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    function getGraphQLType(FieldableField $field, SchemaTypeManager $schemaTypeManager, $nestingLevel = 0)
+    {
+        return Type::listOf(Type::string());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    function getGraphQLInputType(FieldableField $field, SchemaTypeManager $schemaTypeManager, $nestingLevel = 0)
+    {
+        return Type::listOf(Type::string());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    function resolveGraphQLData(FieldableField $field, $value)
+    {
+        return (array)$value;
+    }
+}
