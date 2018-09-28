f24c33284b737bbd42385c2c2e90c4bc9d673b20
Franz Wilding <franz.wilding@me.com>
2018-09-28 10:48:09 +0200

    feature #307 [CoreBundle]: Start refactoring view components

---
+++ src/Bundle/CoreBundle/Resources/webpack/vue/views/BaseViewHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="uk-card-header uk-flex">
+        <h2 class="uk-flex-1">{{ title }}</h2>
+        <a :href="createUrl" class="uk-button uk-button-primary">
+            <span class="uk-margin-small-right-small" v-html="feather.icons['plus'].toSvg()"></span>
+            <i data-feather="plus" class=""></i>
+            {{ createLabel }}
+        </a>
+    </header>
+</template>
+
+<script>
+
+    import feather from 'feather-icons';
+
+    export default {
+        data() {
+            return {
+                feather: feather
+            }
+        },
+        props: [
+            'title',
+            'createLabel',
+            'createUrl'
+        ],
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Bundle/CoreBundle/Resources/webpack/vue/views/BaseViewRow.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="unite-view-row">
+        <div v-for="field in fields"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data() {
+
+            let fields = Object.keys(this.data).map((key) => {
+                return {
+                    key: key,
+                    value: this.data.value,
+                    type: this.resolveType(key),
+                };
+            });
+
+            return {
+                'fields': fields
+            };
+        },
+        props: [
+            'data',
+            'fieldTypes'
+        ],
+        methods: {
+            resolveType() {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
