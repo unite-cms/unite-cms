22fd8f7a19c60d933a5b1248fc18f214f1a645c7
Franz Wilding <franz.wilding@me.com>
2018-10-03 12:22:03 +0200

    feature #307 [CoreBundle]: Working on table styling and auth width matching

--- src/Bundle/CoreBundle/Resources/webpack/vue/views/Fields/Image.vue
+++ src/Bundle/CoreBundle/Resources/webpack/vue/views/Fields/Image.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="view-field view-field-image">
-        <img v-if="src" :src="src" />
+        <span v-if="src" class="uk-inline-clip">
+            <img :src="src" />
+        </span>
     </div>
 </template>
 
@@ -26,6 +28,30 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .view-field-image {
+        height: 50px;
+        width: 80px;
+        text-align: center;
 
+        .uk-inline-clip {
+            width: auto;
+            height: 100%;
+            border-radius: 5px;
+            border: 2px solid white;
+            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.08);
+            margin-top: -2px;
+            margin-left: -2px;
+            cursor: pointer;
+
+            img {
+                height: 100%;
+                width: auto;
+                max-width: none;
+                transform: translateX(-50%);
+                position: relative;
+                left: 50%;
+            }
+        }
+    }
 </style>
--- src/Bundle/CoreBundle/Resources/webpack/vue/views/TableContent.vue
+++ src/Bundle/CoreBundle/Resources/webpack/vue/views/TableContent.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-        <div :key="row.id" v-for="row in rows">
-            <component v-for="(field,identifier) in fields"
-                :key="identifier"
-                :is="$uniteCMSViewFields.resolve(field.type)"
-                :identifier="identifier"
-                :label="field.label"
-                :settings="field.settings"
-                :row="row"></component>
+    <div v-on:click="recalcColumnWidth" class="unite-div-table">
+        <div class="unite-div-table-thead">
+            <div :style="rowStyle" uk-grid class="unite-div-table-row">
+                <div :key="identifier" v-for="(field,identifier) in fields">{{ field.label }}</div>
+            </div>
+        </div>
+        <div class="unite-div-table-tbody">
+            <div :style="rowStyle" uk-grid class="unite-div-table-row" :key="row.id" v-for="row in rows">
+                <div :key="identifier" v-for="(field,identifier) in fields">
+                    <component :is="$uniteCMSViewFields.resolve(field.type)"
+                               :identifier="identifier"
+                               :label="field.label"
+                               :settings="field.settings"
+                               :row="row"></component>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -19,7 +26,40 @@
     export default {
         extends: BaseViewContent,
         data() {
-            return {};
+            return {
+                columnWidth: [],
+            };
+        },
+        updated() {
+            this.recalcColumnWidth();
+        },
+        computed: {
+            rowStyle() {
+                console.log("computed rowStyle");
+                let columns = this.columnWidth.map((column) => {
+                    return 'auto';
+                });
+
+                console.log(this.columnWidth.length);
+                console.log(columns);
+
+                return {
+                    'grid-template-columns': columns.join(' '),
+                }
+            }
+        },
+        methods: {
+            recalcColumnWidth() {
+                console.log("recalcColumnWidth");
+                this.$el.querySelectorAll('.unite-div-table-row').forEach((row) => {
+                    row.childNodes.forEach((column, delta) => {
+                        let width = parseInt(window.getComputedStyle(column).getPropertyValue('width'));
+                        let innerWidth = parseInt(column.firstChild.clientWidth || width);
+                        let fixedWith = Math.abs(width - innerWidth) > 2 ? innerWidth : 0;
+                        this.columnWidth[delta] = this.columnWidth[delta] == null || this.columnWidth[delta] < fixedWith ? fixedWith : this.columnWidth[delta];
+                    });
+                });
+            }
         }
     }
 </script>
