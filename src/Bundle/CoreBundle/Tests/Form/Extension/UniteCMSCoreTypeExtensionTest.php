e8eccb89b9c85eea06321c77e22f4176fc77b1ff
Stefan Kamsker <stefan.kamsker@virtual-identity.com>
2018-11-08 13:54:35 +0100

    fix #239 [CoreBundle]: work on form type extension test

--- src/Bundle/CoreBundle/Tests/Form/Extension/UniteCMSCoreTypeExtensionTest.php
+++ src/Bundle/CoreBundle/Tests/Form/Extension/UniteCMSCoreTypeExtensionTest.php
@@ -8,14 +8,16 @@
 
 namespace UniteCMS\CoreBundle\Tests\Form\Extension;
 
+use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormView;
+use UniteCMS\CoreBundle\Form\Extension\UniteCMSCoreTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Test\TypeTestCase;
-use Symfony\Component\Form\FormView;
-use UniteCMS\CoreBundle\Form\Extension\UniteCMSCoreTypeExtension;
-use Symfony\Component\Form\FormExtensionInterface;
-
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class UniteCMSCoreTypeExtensionTest extends TypeTestCase
 {
@@ -34,19 +36,31 @@ class UniteCMSCoreTypeExtensionTest extends TypeTestCase
         $form = $this->factory->create(IntegerType::class, null, ['not_empty' => true]);
         $this->assertTrue($form->getConfig()->getOption('not_empty'));
 
-        $form = $this->factory->create(TextType::class, null, ['description' => 'hallo']);
-        $this->assertEquals('hallo', $form->getConfig()->getOption('description'));
+        $form = $this->factory->create(FileType::class, null, ['not_empty' => true]);
+        $this->assertTrue($form->getConfig()->getOption('not_empty'));
 
-        /*$this->assertEquals('undefined-tag', $formView->vars['tag']);
-        $this->assertEquals(
-            json_encode(
-                [
-                    'foo' => 'baa',
-                    'foo2' => ['baa'],
-                ]
-            ),
-            $formView->vars['value']
-        );*/
+        $form = $this->factory->create(EmailType::class, null, ['description' => 'bla']);
+        $this->assertEquals('bla', $form->getConfig()->getOption('description'));
+
+        $form = $this->factory->create(RangeType::class, null, ['description' => 'bla1']);
+        $this->assertEquals('bla1', $form->getConfig()->getOption('description'));
+
+        $form = $this->factory->create(TextType::class, null, ['description' => 'bla2']);
+        $this->assertEquals('bla2', $form->getConfig()->getOption('description'));
+
+        // test if description is in form vars
+        $form = $this->createMock(Form::class);
+        $formExtension = new UniteCMSCoreTypeExtension();
+        $formView = new FormView();
+        $formExtension->buildView(
+            $formView,
+            $form,
+            [
+                'description' => 'test123'
+            ]
+        );
+
+        $this->assertEquals('test123', $formView->vars['description']);
     }
 
 }
