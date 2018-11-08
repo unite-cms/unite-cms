98584386d2dace73a1eb98da255dbb596de6efcf
Franz Wilding <franz.wilding@me.com>
2018-11-08 14:34:48 +0100

    fix #239 [CoreBundle]: Fix checkbox settings

--- src/Bundle/CoreBundle/Field/Types/CheckboxFieldType.php
+++ src/Bundle/CoreBundle/Field/Types/CheckboxFieldType.php
@@ -12,7 +12,7 @@ class CheckboxFieldType extends FieldType
 {
     const TYPE = "checkbox";
     const FORM_TYPE = CheckboxType::class;
-    const SETTINGS = ['not_empty', 'description'];
+    const SETTINGS = ['default', 'description'];
 
     /**
      * {@inheritdoc}
