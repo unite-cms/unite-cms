cfade3f2018664a54aa17ff9df8750b10fb7d038
Franz Wilding <franz.wilding@me.com>
2018-12-07 11:50:14 +0100

    fix #323 [StorageBundle]: Add missing OPTIONS method to allow cross-domain access to the sign api

--- src/Bundle/StorageBundle/Controller/SignController.php
+++ src/Bundle/StorageBundle/Controller/SignController.php
@@ -25,7 +25,7 @@ class SignController extends Controller
 
 
     /**
-     * @Route("/content/{content_type}/upload", methods={"POST"})
+     * @Route("/content/{content_type}/upload", methods={"POST", "OPTIONS"})
      * @Entity("contentType", expr="repository.findByIdentifiers(organization, domain, content_type)")
      * @Security("is_granted(constant('UniteCMS\\CoreBundle\\Security\\Voter\\ContentVoter::CREATE'), contentType)")
      *
@@ -62,7 +62,7 @@ class SignController extends Controller
     }
 
     /**
-     * @Route("/setting/{setting_type}/upload", methods={"POST"})
+     * @Route("/setting/{setting_type}/upload", methods={"POST", "OPTIONS"})
      * @Entity("settingType", expr="repository.findByIdentifiers(organization, domain, setting_type)")
      * @Security("is_granted(constant('UniteCMS\\CoreBundle\\Security\\Voter\\SettingVoter::UPDATE'), settingType)")
      *
