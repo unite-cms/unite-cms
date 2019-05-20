b376abe9c3d6dd8a9eb5b5d22b200a675f4a5d75
Franz Wilding <franz.wilding@me.com>
2019-05-20 12:35:41 +0200

    fix #603 [CoreBundle]: Try to move name field to domain accessor table

--- src/Bundle/CoreBundle/Entity/ApiKey.php
+++ src/Bundle/CoreBundle/Entity/ApiKey.php
@@ -24,14 +24,6 @@ class ApiKey extends DomainAccessor implements UserInterface, \Serializable
     /**
      * @var string
      * @Assert\NotBlank(message="not_blank")
-     * @Assert\Length(max="255", maxMessage="too_long")
-     * @ORM\Column(name="name", type="string", length=255)
-     */
-    private $name;
-
-    /**
-     * @var string
-     * @Assert\NotBlank(message="not_blank")
      * @Assert\Length(max="180", maxMessage="too_long")
      * @Assert\Regex(pattern="/^[a-z0-9A-Z\-_]+$/", message="invalid_characters")
      * @ORM\Column(name="token", type="string", length=180, unique=true, nullable=true)
@@ -60,35 +52,6 @@ class ApiKey extends DomainAccessor implements UserInterface, \Serializable
      * @ORM\Column(type="datetime")
      */
     private $created;
-
-    public function __toString()
-    {
-        return ''.$this->getName();
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return ApiKey
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
 
     /**
      * @return Organization
