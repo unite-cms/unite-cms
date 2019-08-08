105a77804be5a9a582ee2dbcea304be9d6088e8a
Kent Nguyen <kent@nguyen>
2019-08-08 11:50:19 +0700

    Create Time field type
    Add a new field type `TimeFieldType` which uses Symfony's core form type `TimeType`. This field is rendered as an input with the type `time`, which includes support for the `min`, `max` and `step` attributes.
    
    Here is an example declaration of the field:
    
    ```
    {
      "title": "Appointment Time",
      "identifier": "appointment_time",
      "type": "time",
      "settings": {
        "description": "",
        "min": "09:00:00",
        "max": "17:00:00",
        "step": 60
      }
    },
    ```

---
+++ src/Bundle/CoreBundle/Field/Types/TimeFieldType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace UniteCMS\CoreBundle\Field\Types;
+
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use UniteCMS\CoreBundle\Entity\FieldableField;
+use UniteCMS\CoreBundle\Field\FieldableFieldSettings;
+use UniteCMS\CoreBundle\Field\FieldType;
+use UniteCMS\CoreBundle\Form\TimeType;
+
+class TimeFieldType extends FieldType
+{
+    const TYPE = "time";
+    const FORM_TYPE = TimeType::class;
+
+    const SETTINGS = ['not_empty', 'description', 'default', 'form_group', 'min', 'max', 'step'];
+
+    /**
+     * {@inheritdoc}
+     */
+    function getFormOptions(FieldableField $field): array
+    {
+        return array_merge(
+            parent::getFormOptions($field),
+            [
+                'widget' => 'single_text',
+                'input' => 'string',
+                'min' => $field->getSettings()->min ?? null,
+                'max' => $field->getSettings()->max ?? null,
+                'step' => $field->getSettings()->step ?? null,
+            ]
+        );
+    }
+
+    protected function validateDefaultValue($value, FieldableFieldSettings $settings, ExecutionContextInterface $context) {
+        $context->getViolations()->addAll(
+            $context->getValidator()->validate($value, new Assert\Time(['message' => 'invalid_initial_data']))
+        );
+    }
+
+    function validateSettings(FieldableFieldSettings $settings, ExecutionContextInterface $context)
+    {
+        parent::validateSettings($settings, $context);
+        $settingsArray = $settings ? get_object_vars($settings) : [];
+
+        if (!empty($settingsArray['min'])) {
+            if ($context->getValidator()
+                    ->validate($settingsArray['min'], new Assert\Time(['message' => 'no_time_value']))
+                    ->count() > 0
+            ) {
+                $context->buildViolation('no_time_value')->atPath('min')->addViolation();
+            }
+        }
+
+        if (!empty($settingsArray['max'])) {
+            if ($context->getValidator()
+                    ->validate($settingsArray['max'], new Assert\Time(['message' => 'no_time_value']))
+                    ->count() > 0
+            ) {
+                $context->buildViolation('no_time_value')->atPath('max')->addViolation();
+            }
+        }
+    }
+
+}
---
+++ src/Bundle/CoreBundle/Tests/Field/TimeFieldTypeTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace UniteCMS\CoreBundle\Tests\Field;
+
+use UniteCMS\CoreBundle\Entity\Content;
+use UniteCMS\CoreBundle\Field\FieldableFieldSettings;
+
+class TimeFieldTypeTest extends FieldTypeTestCase
+{
+    public function testTimeTypeFieldTypeWithEmptySettings()
+    {
+        // Time Type Field with empty settings should be valid.
+        $ctField = $this->createContentTypeField('time');
+        $this->assertCount(0, static::$container->get('validator')->validate($ctField));
+    }
+
+    public function testTimeTypeFieldTypeWithInvalidSettings()
+    {
+        // Time Type Field with invalid settings should not be valid.
+        $ctField = $this->createContentTypeField('time');
+        $ctField->setSettings(new FieldableFieldSettings(['baa' => 'foo']));
+        $errors = static::$container->get('validator')->validate($ctField);
+
+        $this->assertCount(1, $errors);
+        $this->assertEquals('additional_data', $errors->get(0)->getMessageTemplate());
+
+        // test invalid settings
+        $ctField->setSettings(new FieldableFieldSettings(
+            [
+                'bar' => 'foo',
+                'not_empty' => 124
+            ]
+        ));
+
+        $errors = static::$container->get('validator')->validate($ctField);
+        $this->assertCount(2, $errors);
+        $this->assertEquals('additional_data', $errors->get(0)->getMessageTemplate());
+        $this->assertEquals('noboolean_value', $errors->get(1)->getMessageTemplate());
+
+        // test wrong initial data
+        $ctField->setSettings(new FieldableFieldSettings(
+            [
+                'default' => true,
+            ]
+        ));
+
+        $errors = static::$container->get('validator')->validate($ctField);
+        $this->assertCount(1, $errors);
+        $this->assertEquals('invalid_initial_data', $errors->get(0)->getMessageTemplate());
+    }
+
+    public function testTimeTypeFieldTypeWithValidSettings()
+    {
+        $ctField = $this->createContentTypeField('time');
+
+        $ctField->setSettings(new FieldableFieldSettings(
+            [
+                'default' => '08:30:15',
+                'not_empty' => true,
+                'form_group' => 'foo',
+            ]
+        ));
+
+        $errors = static::$container->get('validator')->validate($ctField);
+        echo $errors;
+        $this->assertCount(0, $errors);
+    }
+
+    public function testFormDataTransformers() {
+
+        $ctField = $this->createContentTypeField('time');
+
+        $content = new Content();
+        $id = new \ReflectionProperty($content, 'id');
+        $id->setAccessible(true);
+        $id->setValue($content, 1);
+
+        $form = static::$container->get('unite.cms.fieldable_form_builder')->createForm($ctField->getContentType(), $content, [
+            'csrf_protection' => false,
+        ]);
+
+        $form->submit([
+            $ctField->getIdentifier() => '08:15', // Form input value does not have seconds
+        ]);
+
+        $this->assertEquals('08:15:00', $form->getData()[$ctField->getIdentifier()]);
+
+        $content->setData([
+            $ctField->getIdentifier() => '09:30:45',
+        ]);
+
+        $form = static::$container->get('unite.cms.fieldable_form_builder')->createForm($ctField->getContentType(), $content, [
+            'csrf_protection' => false,
+        ]);
+
+        $this->assertEquals('09:30:45', $form->get($ctField->getIdentifier())->getData());
+    }
+
+    public function testContentFormBuild() {
+
+        $ctField = $this->createContentTypeField('time');
+
+        $ctField->setIdentifier('f1');
+
+        $ctField->getContentType()->getDomain()->getOrganization()->setIdentifier('baa');
+        $ctField->getContentType()->getDomain()->setIdentifier('foo');
+        $ctField->getContentType()->setIdentifier('ct1');
+
+        $ctField->setSettings(new FieldableFieldSettings(
+            [
+                'default' => '09:30:45',
+                'description' => 'blabla'
+            ]
+        ));
+
+        $content = new Content();
+        $content->setContentType($ctField->getContentType());
+
+        $form = static::$container->get('unite.cms.fieldable_form_builder')->createForm(
+            $ctField->getContentType(),
+            $content
+        );
+
+        $formView = $form->createView();
+        $root = $formView->getIterator()->current();
+
+        $this->assertEquals('09:30', $root->vars['value']); // Eliminate seconds after form build
+        $this->assertEquals('blabla', $root->vars['description']);
+
+    }
+}
