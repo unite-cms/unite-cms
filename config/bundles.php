fb06b0841a3e8a802b0d75c866106f071c5f9a5d
Franz Wilding <franz@unite.co.at>
2019-10-31 10:56:02 +0100

    Implement year password reset email send out, requires swiftmailer, twig and transations

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Dunglas\DoctrineJsonOdm\Bundle\DunglasDoctrineJsonOdmBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
--- src/Bundle/CoreBundle/GraphQL/Resolver/EmailPasswordResetResolver.php
+++ src/Bundle/CoreBundle/GraphQL/Resolver/EmailPasswordResetResolver.php
@@ -18,6 +18,7 @@ use UniteCMS\CoreBundle\Event\ContentEvent;
 use UniteCMS\CoreBundle\Field\FieldTypeManager;
 use UniteCMS\CoreBundle\Field\Types\EmailType;
 use UniteCMS\CoreBundle\Field\Types\PasswordType;
+use UniteCMS\CoreBundle\Mailer\PasswordResetMailer;
 use UniteCMS\CoreBundle\Security\User\UserInterface;
 
 class EmailPasswordResetResolver implements FieldResolverInterface
@@ -47,13 +48,19 @@ class EmailPasswordResetResolver implements FieldResolverInterface
      */
     protected $JWTEncoder;
 
-    public function __construct(DomainManager $domainManager, LoggerInterface $uniteCMSDomainLogger, ValidatorInterface $validator, FieldTypeManager $fieldTypeManager, JWTEncoderInterface $JWTEncoder)
+    /**
+     * @var PasswordResetMailer $passwordResetMailer
+     */
+    protected $passwordResetMailer;
+
+    public function __construct(DomainManager $domainManager, LoggerInterface $uniteCMSDomainLogger, ValidatorInterface $validator, FieldTypeManager $fieldTypeManager, JWTEncoderInterface $JWTEncoder, PasswordResetMailer $passwordResetMailer)
     {
         $this->domainManager = $domainManager;
         $this->uniteCMSDomainLogger = $uniteCMSDomainLogger;
         $this->validator = $validator;
         $this->fieldTypeManager = $fieldTypeManager;
         $this->JWTEncoder = $JWTEncoder;
+        $this->passwordResetMailer = $passwordResetMailer;
     }
 
     /**
@@ -65,12 +72,17 @@ class EmailPasswordResetResolver implements FieldResolverInterface
 
     /**
      * {@inheritDoc}
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTEncodeFailureException
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
      */
     public function resolve($value, $args, $context, ResolveInfo $info)
     {
         switch ($info->fieldName) {
-            case 'emailPasswordResetRequest': return $this->resolveRequest($value, $args, $context, $info);
-            case 'emailPasswordResetConfirm': return $this->resolveConfirm($value, $args, $context, $info);
+            case 'emailPasswordResetRequest': return $this->resolveRequest($args);
+            case 'emailPasswordResetConfirm': return $this->resolveConfirm($args);
             default: return null;
         }
     }
@@ -104,12 +116,12 @@ class EmailPasswordResetResolver implements FieldResolverInterface
         $passwordField = $userType->getField($resetDirective['passwordField']);
 
         if(!$emailField) {
-            $this->uniteCMSDomainLogger->warning(sprintf('Unknown emailField "%s" for @emailPasswordReset of user type "%s"', $resetDirective['emailField'], $type));
+            $this->uniteCMSDomainLogger->warning(sprintf('Missing emailField "%s" for @emailPasswordReset of user type "%s"', $resetDirective['emailField'], $type));
             return null;
         }
 
         if(!$passwordField) {
-            $this->uniteCMSDomainLogger->warning(sprintf('Unknown passwordField "%s" for @emailPasswordReset of user type "%s"', $resetDirective['passwordField'], $type));
+            $this->uniteCMSDomainLogger->warning(sprintf('Missing passwordField "%s" for @emailPasswordReset of user type "%s"', $resetDirective['passwordField'], $type));
             return null;
         }
 
@@ -189,22 +201,16 @@ class EmailPasswordResetResolver implements FieldResolverInterface
     }
 
     /**
-     * @param UserInterface $user
-     * @param string $email
-     */
-    protected function sendPasswordRequestEmail(UserInterface $user, string $email) : void {
-        // TODO
-    }
-
-    /**
-     * @param $value
      * @param $args
-     * @param $context
-     * @param \GraphQL\Type\Definition\ResolveInfo $info
      *
      * @return bool
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTEncodeFailureException
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
      */
-    protected function resolveRequest($value, $args, $context, ResolveInfo $info) : bool {
+    protected function resolveRequest($args) : bool {
 
         if(!$config = $this->getDirectiveConfig($args['type'])) {
             return false;
@@ -233,20 +239,24 @@ class EmailPasswordResetResolver implements FieldResolverInterface
         // Persist token
         $domain->getUserManager()->persist($domain, $user, ContentEvent::UPDATE);
 
-        $this->sendPasswordRequestEmail($user, $email);
+        // Send out email
+        if($this->passwordResetMailer->send($config['resetUrl'], $user->getPasswordResetToken(), $email) === 0) {
+            $this->uniteCMSDomainLogger->error(sprintf('Could not send out password reset email to user with username "%s".', $args['username']));
+            return false;
+        }
+
+        // Replay to user
         $this->uniteCMSDomainLogger->info(sprintf('User with username "%s" requested a password reset.', $args['username']));
         return true;
     }
 
     /**
-     * @param $value
      * @param $args
-     * @param $context
-     * @param \GraphQL\Type\Definition\ResolveInfo $info
      *
      * @return bool
+     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException
      */
-    protected function resolveConfirm($value, $args, $context, ResolveInfo $info) : bool {
+    protected function resolveConfirm($args) : bool {
 
         if(!$config = $this->getDirectiveConfig($args['type'])) {
             return false;
@@ -277,9 +287,10 @@ class EmailPasswordResetResolver implements FieldResolverInterface
          */
         $passwordField = $this->fieldTypeManager->getFieldType(PasswordType::getType());
 
-        $domain->getUserManager()->update($domain, $user, [
-            $config['passwordField'] => $passwordField->normalizePassword($user, $args['password']),
-        ]);
+        // Update user password field.
+        $data = $user->getData();
+        $data[$config['passwordField']] = $passwordField->normalizePassword($user, $args['password']);
+        $domain->getUserManager()->update($domain, $user, $data);
 
         // Only validate the single password field.
         $context = new ExecutionContext($this->validator, $user, new class() implements TranslatorInterface, LocaleAwareInterface {
