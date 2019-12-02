92c7c5937e514c7da8c4e41ffa950d204c78789c
Stefan Kamsker <stefan.kamsker@virtual-identity.com>
2019-12-02 19:11:22 +0100

    work on media bundle

--- src/Bundle/MediaBundle/GraphQL/Resolver/PreSignUrlResolver.php
+++ src/Bundle/MediaBundle/GraphQL/Resolver/PreSignUrlResolver.php
@@ -6,6 +6,10 @@ namespace UniteCMS\MediaBundle\GraphQL\Resolver;
 use GraphQL\Language\AST\ObjectTypeDefinitionNode;
 use GraphQL\Type\Definition\ResolveInfo;
 use UniteCMS\CoreBundle\GraphQL\Resolver\Field\FieldResolverInterface;
+use Etime\Flysystem\Plugin\AWS_S3 as AWS_S3_Plugin;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+use Aws\S3\S3Client;
 
 class PreSignUrlResolver implements FieldResolverInterface
 {
@@ -25,6 +29,24 @@ class PreSignUrlResolver implements FieldResolverInterface
         if($info->fieldName !== 'uniteMediaPreSignUrl') {
             return null;
         }
+
+        $client = new S3Client([
+            'credentials' => [
+                'key'    => 'your-key',
+                'secret' => 'your-secret'
+            ],
+            'region' => 'your-region',
+            'version' => 'latest|version',
+        ]);
+
+        $adapter = new AwsS3Adapter($client, 'your-bucket-name');
+        $filesystem = new Filesystem($adapter);
+
+        $filesystem->addPlugin(new AWS_S3_Plugin\PresignedUrl());
+
+        $success = $filesystem->getPresignedUrl('/tmp');
+        dump($success);
+        exit;
 
         return "TODO";
     }
