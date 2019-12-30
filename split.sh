b60a7c1d34ac372eb02051ddf1406f30ce24e4f9
Franz Wilding <franz@unite.co.at>
2019-12-30 17:08:18 +0100

    splitsh-lite does not work with tags atm

--- split.sh
+++ split.sh
@@ -3,13 +3,13 @@
 # First draft of auto code split based on https://github.com/splitsh/lite
 
 SHA1=`splitsh-lite --prefix=src/Bundle/AdminBundle/`
-git push git@github.com:unite-cms/AdminBundle.git ${SHA1}:master --tags
+git push git@github.com:unite-cms/AdminBundle.git ${SHA1}:master
 
 SHA1=`splitsh-lite --prefix=src/Bundle/CoreBundle/`
-git push git@github.com:unite-cms/CoreBundle.git ${SHA1}:master --tags
+git push git@github.com:unite-cms/CoreBundle.git ${SHA1}:master
 
 SHA1=`splitsh-lite --prefix=src/Bundle/DoctrineORMBundle/`
-git push git@github.com:unite-cms/DoctrineORMBundle.git ${SHA1}:master --tags
+git push git@github.com:unite-cms/DoctrineORMBundle.git ${SHA1}:master
 
 SHA1=`splitsh-lite --prefix=src/Bundle/MediaBundle/`
-git push git@github.com:unite-cms/MediaBundle.git ${SHA1}:master --tags
+git push git@github.com:unite-cms/MediaBundle.git ${SHA1}:master
