7c24982793a74345bc1d1cba7b3304b118b28145
Franz Wilding <franz@unite.co.at>
2019-12-30 12:20:15 +0100

    feature [CoreBundle] Improve create user command

--- src/Bundle/CoreBundle/Command/CreateUserCommand.php
+++ src/Bundle/CoreBundle/Command/CreateUserCommand.php
@@ -2,17 +2,20 @@
 
 namespace UniteCMS\CoreBundle\Command;
 
+use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use UniteCMS\CoreBundle\Content\SensitiveFieldData;
+use UniteCMS\CoreBundle\ContentType\UserType;
+use UniteCMS\CoreBundle\Domain\Domain;
 use UniteCMS\CoreBundle\Domain\DomainManager;
-use UniteCMS\CoreBundle\Event\ContentEvent;
 use UniteCMS\CoreBundle\EventSubscriber\SetCurrentDomainSubscriber;
 use UniteCMS\CoreBundle\GraphQL\SchemaManager;
 use UniteCMS\CoreBundle\Security\Encoder\FieldableUserPasswordEncoder;
@@ -62,14 +65,72 @@ class CreateUserCommand extends Command
         $this
             ->setName('unite:user:create')
             ->setDescription('Create a new unite cms user')
-            ->addArgument('type', InputArgument::REQUIRED)
-            ->addArgument('username', InputArgument::REQUIRED)
-            ->addArgument('password', InputArgument::REQUIRED)
-            ->addOption('persist', InputOption::VALUE_NONE)
+            ->addOption('type', 'type', InputOption::VALUE_REQUIRED)
+            ->addOption('username', 'user', InputOption::VALUE_REQUIRED)
+            ->addOption('password', 'pass', InputOption::VALUE_REQUIRED)
+            ->addOption('persist', 'per', InputOption::VALUE_NONE)
 
             // Will be used in SetCurrentDomainSubscriber
             ->addOption(SetCurrentDomainSubscriber::COMMAND_OPTION, '', InputOption::VALUE_OPTIONAL, 'Specify the unite domain id to set before executing the command.')
         ;
+    }
+
+    /**
+     * @param Domain $domain
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return array
+     */
+    protected function getOptions(Domain $domain, InputInterface $input, OutputInterface $output) : array {
+
+        $helper = $this->getHelper('question');
+
+        // Get type:
+        $type = $input->getOption('type');
+
+        if($type) {
+            if(!$domain->getContentTypeManager()->getUserType($type)) {
+                throw new InvalidArgumentException(sprintf('No user type "%s" found in the current schema.', $type));
+            }
+
+        } else {
+            $question = new ChoiceQuestion('Please select a UniteUser type:', array_map(function(UserType $userType){
+                return $userType->getId();
+            }, $domain->getContentTypeManager()->getUserTypes()));
+            $question->setMultiselect(false);
+            $type = $helper->ask($input, $output, $question);
+        }
+
+        // Get username:
+        $username = $input->getOption('username');
+        if(!$username) {
+            $question = new Question('Please select a username:');
+            $question->setValidator(function ($value) {
+                if (trim($value) == '') {
+                    throw new \Exception('The username cannot be empty');
+                }
+
+                return $value;
+            });
+            $username = $helper->ask($input, $output, $question);
+        }
+
+        // Get password:
+        $password = $input->getOption('password');
+        if(!$password) {
+            $question = new Question('Please select a password:');
+            $question->setValidator(function ($value) {
+                if (trim($value) == '') {
+                    throw new \Exception('The password cannot be empty');
+                }
+
+                return $value;
+            });
+            $question->setHidden(true);
+            $password = $helper->ask($input, $output, $question);
+        }
+
+        return [$type, $username, $password];
     }
 
     /**
@@ -80,17 +141,9 @@ class CreateUserCommand extends Command
         $this->tokenStorage->setToken(new AnonymousToken('', ''));
         $this->schemaManager->buildCacheableSchema();
         $domain = $this->domainManager->current();
-        $userType = $domain->getContentTypeManager()->getUserType($input->getArgument('type'));
 
-        if(!$userType) {
-            $output->writeln(sprintf(
-                '<error>No user type "%s" found. Please use one of [%s] or update your schema.</error>',
-                $input->getArgument('type'),
-                join(', ', array_keys($domain->getContentTypeManager()->getUserTypes()))
-            ));
-            return;
-        }
-
+        list($type, $username, $password) = $this->getOptions($domain, $input, $output);
+        $userType = $domain->getContentTypeManager()->getUserType($type);
         $passwordField = null;
 
         foreach ($userType->getDirectives() as $directive) {
@@ -102,15 +155,15 @@ class CreateUserCommand extends Command
         if(!$passwordField) {
             $output->writeln(sprintf(
                 '<error>@passwordAuthenticator was not configured for GraphQL user type "%s".</error>',
-                $input->getArgument('type')
+                $type
             ));
             return;
         }
 
-        $user = $domain->getUserManager()->create($domain, $input->getArgument('type'));
+        $user = $domain->getUserManager()->create($domain, $type);
         $domain->getUserManager()->update($domain, $user, [
-            'username' => new SensitiveFieldData($input->getArgument('username')),
-            $passwordField => new SensitiveFieldData($this->passwordEncoder->encodePassword($user, $input->getArgument('password'))),
+            'username' => new SensitiveFieldData($username),
+            $passwordField => new SensitiveFieldData($this->passwordEncoder->encodePassword($user, $password)),
         ]);
 
         $errors = $this->validator->validate($user);
