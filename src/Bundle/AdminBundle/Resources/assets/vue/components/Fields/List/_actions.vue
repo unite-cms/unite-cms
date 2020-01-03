635bd22cf2fe05d1d640d31fca40f248c8b67436
Franz Wilding <franz@unite.co.at>
2020-01-03 16:57:16 +0100

    fix #745 [AdminBundle]

--- src/Bundle/AdminBundle/Resources/assets/vue/components/Fields/List/_actions.vue
+++ src/Bundle/AdminBundle/Resources/assets/vue/components/Fields/List/_actions.vue
@@ -1,32 +1,32 @@
 <template>
-  <ul class="uk-iconnav uk-flex-right">
-    <li v-if="!deleted && is_granted('user_invite')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('user_invite')" :title="$t('content.list.actions.user_invite')" class="uk-text-success"><icon name="send" /></router-link></li>
-    <li v-if="!deleted && is_granted('update')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('update')" :title="$t('content.list.actions.update')"><icon name="edit-2" /></router-link></li>
-    <li v-if="!deleted && is_granted('update')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('translate')" :title="$t('content.list.actions.translate')"><icon name="globe" /></router-link></li>
-    <li v-if="!deleted && is_granted('update')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('revert')" :title="$t('content.list.actions.revert')"><icon name="rotate-ccw" /></router-link></li>
-    <li v-if="!deleted && is_granted('delete')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('delete')" :title="$t('content.list.actions.delete')" class="uk-text-danger"><icon name="trash-2" /></router-link></li>
+    <ul class="uk-iconnav uk-flex-right">
+        <li v-if="!deleted && is_granted('user_invite')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('user_invite')" :title="$t('content.list.actions.user_invite')" class="uk-text-success"><icon name="send" /></router-link></li>
+        <li v-if="!deleted && is_granted('update')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('update')" :title="$t('content.list.actions.update')"><icon name="edit-2" /></router-link></li>
+        <li v-if="view.translatable && !deleted && is_granted('update')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('translate')" :title="$t('content.list.actions.translate')"><icon name="globe" /></router-link></li>
+        <li v-if="!deleted && is_granted('update')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('revert')" :title="$t('content.list.actions.revert')"><icon name="rotate-ccw" /></router-link></li>
+        <li v-if="!deleted && is_granted('delete')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('delete')" :title="$t('content.list.actions.delete')" class="uk-text-danger"><icon name="trash-2" /></router-link></li>
 
-    <li v-if="deleted && is_granted('update')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('recover')" :title="$t('content.list.actions.recover')" class="uk-text-success"><icon name="rewind" /></router-link></li>
-    <li v-if="deleted && is_granted('permanent_delete')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('permanent_delete')" :title="$t('content.list.actions.permanent_delete')" class="uk-text-danger"><icon name="x-circle" /></router-link></li>
-  </ul>
+        <li v-if="deleted && is_granted('update')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('recover')" :title="$t('content.list.actions.recover')" class="uk-text-success"><icon name="rewind" /></router-link></li>
+        <li v-if="deleted && is_granted('permanent_delete')"><router-link uk-tooltip :target="embedded ? '_blank' : '_self'" :to="to('permanent_delete')" :title="$t('content.list.actions.permanent_delete')" class="uk-text-danger"><icon name="x-circle" /></router-link></li>
+    </ul>
 </template>
 <script>
-  import Icon from "../../Icon";
-  import _abstract from "./_abstract";
+    import Icon from "../../Icon";
+    import _abstract from "./_abstract";
 
-  export default {
-      extends: _abstract,
-      components: { Icon },
-  }
+    export default {
+        extends: _abstract,
+        components: { Icon },
+    }
 </script>
 <style scoped lang="scss">
-  .uk-iconnav {
-    flex-wrap: nowrap;
+    .uk-iconnav {
+        flex-wrap: nowrap;
 
-    li {
-      a {
-        width: 20px;
-      }
+        li {
+            a {
+                width: 20px;
+            }
+        }
     }
-  }
 </style>
