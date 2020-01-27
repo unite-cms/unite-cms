ad1195429b2273119bf99f85be341bd4f36dec71
Franz Wilding <franz@unite.co.at>
2020-01-27 18:38:48 +0100

    Trying out different approches to define block types

--- src/Bundle/CoreBundle/Field/Types/PageBuilderFieldType.php
+++ src/Bundle/CoreBundle/Field/Types/PageBuilderFieldType.php
@@ -3,50 +3,49 @@
 namespace UniteCMS\CoreBundle\Field\Types;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use UniteCMS\AdminBundle\AdminView\AdminViewTypeManager;
+use GraphQL\Type\Definition\ObjectType;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use UniteCMS\CoreBundle\Content\ContentInterface;
 use UniteCMS\CoreBundle\Content\FieldData;
+use UniteCMS\CoreBundle\ContentType\ContentType;
 use UniteCMS\CoreBundle\ContentType\ContentTypeField;
-use UniteCMS\CoreBundle\Domain\DomainManager;
-use UniteCMS\CoreBundle\Expression\SaveExpressionLanguage;
 
 class PageBuilderFieldType extends AbstractFieldType
 {
     const TYPE = 'pageBuilder';
-    const GRAPHQL_INPUT_TYPE = 'UnitePageBuilderInput';
+    const GRAPHQL_INPUT_TYPE = 'JSON';
 
-    /**
-     * @var DomainManager $domainManager
-     */
-    protected $domainManager;
 
-    /**
-     * @var AdminViewTypeManager $adminViewTypeManager
-     */
-    protected $adminViewTypeManager;
-
-    public function __construct(SaveExpressionLanguage $expressionLanguage, DomainManager $domainManager, AdminViewTypeManager $adminViewTypeManager)
-    {
-        parent::__construct($expressionLanguage);
-        $this->domainManager = $domainManager;
-        $this->adminViewTypeManager = $adminViewTypeManager;
-    }
 
     /**
      * {@inheritDoc}
      */
-    protected function allowedReturnTypes(ContentTypeField $field) {
-        return ['UnitePageBuilderResult'];
+    public function validateFieldDefinition(ContentType $contentType, ContentTypeField $field, ExecutionContextInterface $context) : void {
+
+        $returnTypes = empty($field->getUnionTypes()) ? [$field->getReturnType()] : array_keys($field->getUnionTypes());
+
+        // TODO: Validate interface.
+        //dump($returnTypes);
+
+        // Validate return type.
+        /*$allowedTypes = $this->allowedReturnTypes($field);
+        if(!in_array($field->getReturnType(), $allowedTypes)) {
+            $context
+                ->buildViolation('Invalid GraphQL return type "{{ return_type }}" for field of type "{{ type }}". Please use on of [{{ allowed_return_types }}].')
+                ->setParameter('{{ type }}', static::getType())
+                ->setParameter('{{ return_type }}', $field->getReturnType())
+                ->setParameter('{{ allowed_return_types }}', join(', ', $allowedTypes))
+                ->addViolation();
+            return;
+        }*/
     }
 
     /**
      * {@inheritDoc}
      */
     protected function resolveRowData(ContentInterface $content, ContentTypeField $field, FieldData $fieldData, array $args = []) {
-        return [
-            'rendered' => $fieldData->resolveData('content', ''),
-            'nodes' => $fieldData->resolveData('nodes', []),
-        ];
+        // TODO
+        return null;
     }
 
     /**
@@ -54,9 +53,9 @@ class PageBuilderFieldType extends AbstractFieldType
      */
     public function getPublicSettings(ContentTypeField $field) : ?ArrayCollection {
         return new ArrayCollection([
-            'customBlocks' => $field->getSettings()->get('customBlocks', []),
+            /*'customBlocks' => $field->getSettings()->get('customBlocks', []),
             'mediaType' => null,
-            'templateType' => null,
+            'templateType' => null,*/
         ]);
     }
 }
